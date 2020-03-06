3217c638ea1bb11a76954f68b4026ebfdf561763
codershona <falgunimst95@gmail.com>
2020-03-06 11:02:37 +0100

    working on forecast javascript

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # simple-weather-app
 ### Build a Simple Weather App with Vanilla JS
+
+
+
+* Notes :
+```
+    - Simple API to get the data , go to this link from browsers (https://geoip-db.com/json/) and 
+```
--- js/app.js
+++ js/app.js
@@ -1,1 +1,54 @@
 // function
+// declaring some variables that hold the element .
+// showing my fetch data
+// these are all html elements
+
+let name = document.querySelector(".name");
+let date = document.querySelector(".date");
+let temp = document.querySelector(".temp");
+let desc = document.querySelector(".desc");
+let hum = document.querySelector(".hum");
+let pressure = document.querySelector(".pressure");
+let ozone = document.querySelector(".ozone");
+let icon = document.querySelector(".image");
+
+// two more variables,
+
+let temps = [];
+let isConverted = false;
+
+// using ES6 class syntax to create object
+
+// create a simple object and methods to work with
+
+class Weather {
+
+  // first method : giving my location name
+
+  // Dark sky doesnot provide location name ...
+  // Adding APi to get locations name ...
+
+  // Using simple API to get the data...
+  //  This API returns location details from our IP address
+
+  getLocationName() {
+    // using fetch API to get AJAX calls you can use XHP also
+    fetch("https://geoip-db.com/json/")
+    .then(res => res.json())
+    .then(res => {
+      name.innerHTML += `${res.city}, ${res.state}`;
+
+    });
+  }
+  // making uses of darksky API to get current forecast
+
+  getCurrentForecast() {
+    
+  }
+
+}
+
+
+// instatiate the class
+var start = new Weather();
+start.getLocationName();
